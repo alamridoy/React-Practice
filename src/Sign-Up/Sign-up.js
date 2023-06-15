91ade2dc235353e2bf776b2df3aa77d55b536bc2
alam ridoy <alamridoy7@gmail.com>
2023-06-15 12:08:18 +0600

    using formik and yup

--- src/Sign-Up/Sign-up.js
+++ src/Sign-Up/Sign-up.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Formik, useFormik } from 'formik';
+import { Formik, useFormik, validateYupSchema } from 'formik';
+import { object, string, number, date, InferType } from 'yup';
 
 const SignUp = () => {
   const initialValues = {
@@ -8,16 +9,31 @@ const SignUp = () => {
     password: ''
   };
 
+  
   const onSubmit = (values, { resetForm }) => {
     console.log(values);
     resetForm({values:""});
   };
 
-  const formik = useFormik({
-    initialValues,
-    onSubmit
-  });
 
+  let validationSchema = object({
+    name: string().required().min(3,"min 2 character").max(10,"max 10 char"),  // yup form system assign
+    email: string().email(),
+    password: string().min(6,"pass have min 6 character"),
+   
+  });
+  
+  const formik = useFormik({       // assign useFormik to formik and assign properties
+    initialValues,
+    onSubmit,
+    validationSchema
+  });
+  console.error(formik.errors);
+  
+  const nameError = formik.touched.name && formik.errors.name && <span>{formik.errors.name}</span>
+
+   const emailError = formik.touched.email && formik.errors.email && <span>{formik.errors.email}</span>
+const passwordError = formik.touched.password && formik.errors.password && <span>{formik.errors.password}</span>
   return (
     <div>
       <h1>Control form data using Formik package</h1>
@@ -31,6 +47,7 @@ const SignUp = () => {
             value={formik.values.name}
             onChange={formik.handleChange}
           />
+          {nameError}
         </div>
         <div>
           <label htmlFor="email">Email:</label>
@@ -41,6 +58,7 @@ const SignUp = () => {
             value={formik.values.email}
             onChange={formik.handleChange}
           />
+           {emailError}
         </div>
         <div>
           <label htmlFor="password">Password:</label>
@@ -51,6 +69,7 @@ const SignUp = () => {
             value={formik.values.password}
             onChange={formik.handleChange}
           />
+            {passwordError}
         </div>
         <button type="submit">Signup</button>
       </form>
